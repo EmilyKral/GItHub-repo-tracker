147bf580df50a3268aa1a506590d40b8462bf9fe
EmilyKral <emilykral@gmail.com>
2022-01-12 13:01:52 +0000

    updated bio to display more info Co-authored-by: Souheil Fenghour <sf308@imperial.ac.uk>

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,29 +1,14 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Username } from "../../contexts";
-import axios from "axios";
+import React from "react";
 
-const Bio = () => {
-	const [RepoData, setRepoData] = useState("");
-
-	const username = useContext(Username);
-
-	useEffect(() => {
-		async function getRepoData() {
-			// setStatusMessage("Retrieving Data");
-			try {
-				let data = await axios.get(`https://api.github.com/users/${username}/repos/`);
-				setRepoData(data);
-			} catch (err) {
-				console.warn(err);
-				// setStatusMessage(`Oops there\'s been an issue! ${err.message}`);
-			}
-		}
-		getRepoData();
-	}, [username]);
-
+const Bio = userData => {
 	return (
 		<div>
-			<p>{RepoData}</p>
+			<p>paragraph</p>
+			<ul>
+				<li>No. of repositories: {userData.public_repos} </li>
+				<li>Followers: {userData.followers} </li>
+				<li>Following: {userData.following} </li>
+			</ul>
 		</div>
 	);
 };
