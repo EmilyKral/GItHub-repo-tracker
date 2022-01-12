a7d4272f716786735b78a6f92d07e1a45571fe74
EmilyKral <emilykral@gmail.com>
2022-01-12 15:43:22 +0000

    removed reducer code since it is no longer being used

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-import { searchReducer } from "./reducers";
-
-const store = createStore(searchReducer, composeWithDevTools());
-
-export default store;
